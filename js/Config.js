522043bc569e3c4edb6708d31b804e8c56b5d3e4
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2020-01-31 21:35:57 +0000

    tidied JS into folders

---
+++ js/Config.js
@@ -0,0 +1,17 @@
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
