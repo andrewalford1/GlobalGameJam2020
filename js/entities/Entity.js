e187265ed044daa3a19fb8c5dce321398c1edf7d
Andrew Alford <Andrew.Alford@tracerco.com>
2020-02-04 21:21:02 +0000

    Created the Entity class

---
+++ js/entities/Entity.js
@@ -0,0 +1,77 @@
+class Entity
+{
+    _posX;
+    _posY;
+    _scale;
+    _metadata
+    _entity;
+    
+    constructor()
+    {
+        //Define this class as abstract.
+        if (this.constructor === Entity)
+        {
+            throw new console.error(
+                'Cannot instansiate abstract class: ' + 'Entiy'
+            );
+        }
+    }
+
+    Preload(gameConfig)
+    {
+        throw new console.error(
+            'Abstract method Entity.Preload() must be overridden'
+        );
+    }
+
+    Create(gameConfig)
+    {
+        throw new console.error(
+            'Abstract method Entity.Create() must be overridden'
+        );
+    }
+
+    Update(time, delta)
+    {
+        throw new console.error(
+            'Abstract method Entity.Update() must be overriden'
+        );
+    }
+
+    GetXPos()
+    {
+        return this._posX;
+    }
+
+    GetYPos()
+    {
+        return this._posY;
+    }
+
+    GetScale()
+    {
+        return this._scale;
+    }
+
+    Get()
+    {
+        return this._entity;
+    }
+
+    SetXPos(x)
+    {
+        this._posX = x;
+    }
+
+    SetYPos(y)
+    {
+        this._posY = y;
+    }
+
+    SetScale(scale)
+    {
+        this._scale = scale;
+    }
+}
+
+export default Entity;
